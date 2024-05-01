330e8434d449555142f0a36340eb0668cd6269c2
lindseymaddox9 <153481027+lindseymaddox9@users.noreply.github.com>
2024-05-01 08:40:24 -0500

    finished product

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Circle } = require("./lib/shapes");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-        type: 'list', 
-        name: 'shapeChoice', 
-        message: 'Please choose a shape',
-        choices: ['Circle', 'Triangle', 'Square']
+      type: "list",
+      name: "shapeChoice",
+      message: "Please choose a shape",
+      choices: ["Circle", "Triangle", "Square"],
     },
     {
-        type: 'input', 
-        name: 'text', 
-        message: 'what do you want your logo to say'
+      type: "input",
+      name: "text",
+      message: "What do you want the logo to say",
     },
     {
-        type: 'input', 
-        name: 'shapeColor', 
-        message: 'what do you want your shape color to be'
+      type: "input",
+      name: "shapeColor",
+      message: "What do you want the shape color to be?",
     },
     {
-        type: 'input', 
-        name: 'textColor', 
-        message: 'what do you want your text color to be?'
+      type: "input",
+      name: "textColor",
+      message: "What do you want the text color to be?",
     },
-]).then((answers)=> {
+  ])
+  .then((answers) => {
+    switch (answers.shapeChoice) {
+      case "Circle":
+        const circle = new Circle(
+          answers.text,
+          answers.textColor,
+          answers.shapeColor
+        );
+        fs.writeFile("logo.svg", circle.render(), (err) => {
+          if (err) console.log(err);
+          console.log("generated logo.svg");
+        });
+        break;
+      case "Triangle":
+        const triangle = new Triangle(
+          answers.text,
+          answers.textColor,
+          answers.shapeColor
+        );
+        fs.writeFile("logo.svg", triangle.render(), (err) => {
+          if (err) console.log(err);
+          console.log("generated logo.svg");
+        });
+        break;
+      case "Square":
+        const square = new Square(
+          answers.text,
+          answers.textColor,
+          answers.shapeColor
+        );
+        fs.writeFile("logo.svg", square.render(), (err) => {
+          if (err) console.log(err);
+          console.log("generated logo.svg");
+        });
+        break;
+    }
+
     console.log(answers);
-    
-    const circle = new Circle(answers.text, answers.textColor, answers.shapeColor)
-  
-    fs.writeFile('logo.svg', circle.render(), (err)=> {
-        if(err) console.log(err);
-        console.log('file created');
-    })
-
-})
+  });
